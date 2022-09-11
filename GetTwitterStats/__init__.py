415317517289b77dc4dbc7246478d0f3025225bb
Jess Pomfret <jesspomfret@datamasterminds.io>
2022-09-11 20:03:18 +0100

    add the code

---
+++ GetTwitterStats/__init__.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import logging
+import datetime
+import os
+import azure.functions as func
+
+def main(req: func.HttpRequest, twitterstats: func.Out[func.SqlRow]) -> func.HttpResponse:
+    # Note that this expects the body to be a JSON object which
+    # have a property matching each of the columns in the table to upsert to.
+
+    # get the date
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    # call the twitter api and save the data to the db
+    token = os.environ["TwitterToken"]
+    r =requests.get("https://api.twitter.com/2/users/by?usernames=jpomfret&user.fields=created_at%2Cpublic_metrics", headers={"Authorization": "Bearer {}".format(token)})
+    robj = r.json()
+    data = robj['data']
+    stats = data[0]
+    dbRow = {'collectionDate': dt_string, 'name' : stats['name'], 'username': stats['username'], 'id': stats['id'], 'followersCount': stats['public_metrics']['followers_count'], 'followingCount': stats['public_metrics']['following_count'], 'tweetCount': stats['public_metrics']['tweet_count'], 'listedCount': stats['public_metrics']['listed_count']}
+    row = func.SqlRow.from_dict(dbRow)
+    twitterstats.set(row)
+
+    func.HttpResponse(f"Hello. This HTTP triggered function executed successfully.")
+
+    return func.HttpResponse(
+        body=req.get_body(),
+        status_code=201,
+        mimetype="application/json"
+    )
