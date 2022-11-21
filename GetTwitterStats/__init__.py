e3a913dab35ed7ac04707ddcbf573364a20620ef
jpomfret <jpomfret7@gmail.com>
2022-11-21 14:53:14 +0000

    add mast follower counts

--- GetTwitterStats/__init__.py
+++ GetTwitterStats/__init__.py
@@ -13,13 +13,21 @@ def main(timetrig: func.TimerRequest, twitterstats: func.Out[func.SqlRow]) -> No
     now = datetime.datetime.now()
     dt_string = now.strftime('%Y-%m-%dT%H:%M:%S')
 
-    # call the twitter api and save the data to the database
+    # call the twitter api
     token = os.environ["TwitterToken"]
     r =requests.get("https://api.twitter.com/2/users/by?usernames=jpomfret&user.fields=created_at%2Cpublic_metrics", headers={"Authorization": "Bearer {}".format(token)})
     robj = r.json()
     data = robj['data']
     stats = data[0]
-    dbRow = {'collectionDate': dt_string, 'name' : stats['name'], 'username': stats['username'], 'id': stats['id'], 'followersCount': stats['public_metrics']['followers_count'], 'followingCount': stats['public_metrics']['following_count'], 'tweetCount': stats['public_metrics']['tweet_count'], 'listedCount': stats['public_metrics']['listed_count']}
+
+    # call the mastadon api
+    tokenMast = os.environ["MastadonToken"]
+    rMast =requests.get("https://tech.lgbt/api/v1/accounts/109304329691833749/", headers={"Authorization": "Bearer {}".format(tokenMast)})
+    robjMast = rMast.json()
+    dataMast = robjMast['accounts']
+    statsMast = dataMast[0]
+
+    # save the data to the database
+    dbRow = {'collectionDate': dt_string, 'name' : stats['name'], 'username': stats['username'], 'id': stats['id'], 'followersCount': stats['public_metrics']['followers_count'], 'followingCount': stats['public_metrics']['following_count'], 'tweetCount': stats['public_metrics']['tweet_count'], 'listedCount': stats['public_metrics']['listed_count'],'MastURL': statsMast['url'], 'MastUsername': statsMast['username'], 'MastFollowers': statsMast['followers_count'],'MastFollowing': statsMast['following_count'], 'MastStatusCount': statsMast['statuses_count']}
     row = func.SqlRow.from_dict(dbRow)
     twitterstats.set(row)
-
